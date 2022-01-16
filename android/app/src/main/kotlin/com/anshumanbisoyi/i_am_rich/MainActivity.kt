98e6c3156eda378f2f16ae83aa8d1475f26236df
Anshuman Bisoyi <ansuman746@gmail.com>
2022-01-16 22:18:05 +0530

    intial

---
+++ android/app/src/main/kotlin/com/anshumanbisoyi/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anshumanbisoyi.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//THe main funtion is the starting point for all flutter apps.
+void main() {
+  runApp(
+    //widget tree
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
